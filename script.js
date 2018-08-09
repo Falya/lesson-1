2a6bb3a78b01bb7fe0468aecdd77f5b446870a2f
Maksim <anger65@gmail.com>
2018-08-10 00:18:27 +0300

    Первое задание

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-{
-	let yourBudget = prompt("Ваш бюджет на месяц?", "");
-	let shopName = prompt("Название вашего магазина?", "");
-	mainList = {
+let yourBudget = prompt("Ваш бюджет на месяц?", "");
+let shopName = prompt("Название вашего магазина?", "");
+let	mainList = {
 		budget: yourBudget,
 		shop: shopName,
 		shopGoods: [],
 		employers: {},
 		open: true
-	}
-	for (var i = 0; i < 3; i++) {
-				mainList.shopGoods.push( prompt("Какой тип товаров будем продавать?", "") );
-			}
+	};
 
-	alert( "Ваш бюджет на 1 день составляет: " + (mainList.budget/30).toFixed(1) );
+	for (let i = 0; i < 3; i++) {
+				mainList.shopGoods.push( prompt("Какой тип товаров будем продавать?", "") );
+	}
+
+	alert( "Ваш бюджет на 1 день составляет: " + (mainList.budget / 30).toFixed(1) );
 	console.log(mainList);
-}
